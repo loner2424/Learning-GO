d82f3351dcd71530bfdad573a85bd0bdc7ecf303
loner2424 <sahilsobhani4@gmail.com>
2024-03-16 01:41:00 +0530

    Constants added

---
+++ Basics/Constants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func constants() {
+
+	const num int = 54
+	const name string = "Sahil Sobhani"
+	pi := 3.14
+
+	fmt.Printf("Here the number: %d the string name: %s and the value pi: %f are all constants and their value cannot be changed", num, name, pi)
+
+}
--- Basics/Variables.go
+++ Basics/Variables.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func variables() {
 
 	var number1 int
 	var number2 int = 3
@@ -14,6 +14,6 @@ func main() {
 	fmt.Printf("Value of number2 is initialized and the type is mentioned, it is %d and an integer\n", number2)
 	fmt.Printf("Value of number3 is initialized in a shorter manner and the type is inferred, therefore it is %d\n", number3)
 	fmt.Printf("StringExample is a string example, type is inferred, value is %s\n", StringExample)
-	fmt.Printf("StringExample is a bool example, type is inferred, value is %t\n", BoolExample)
+	fmt.Printf("StringExample is a bool example, type is inferred, value is %t\n\n", BoolExample)
 
 }
